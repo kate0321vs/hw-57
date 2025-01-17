4ceb012ba55c210d187ff335b82ddc23b405ba6f
kate0321vs <ekaterina.chechulina2103@gmail.com>
2025-01-17 09:09:21 +0600

    Добавила компонент для от правки данных формы

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import './App.css'
+import UserForm from './components/UserForm/UserForm.tsx';
+import { User } from './types';
+import { useState } from 'react';
 
 const App = () => {
 
+  const [user, setUser] = useState<User[]>([])
+
+  const addUser = (newUser: User) => {
+    setUser(prevUser => [newUser, ...prevUser]);
+    console.log(user)
+  }
+
   return (
     <>
+      <header className="mb-5">
 
+      </header>
+      <main className="container">
+        <div className="row">
+          <div className="col-4">
+            <UserForm onSubmitFormToAddUser={addUser}/>
+          </div>
+          <div className="col-4">
+            Users
+          </div>
+        </div>
+      </main>
     </>
   )
 };
---
+++ src/components/UserForm/UserForm.tsx
@@ -0,0 +1,91 @@
+import { User, UserMutation } from '../../types';
+import { useState } from 'react';
+
+interface Props {
+  onSubmitFormToAddUser: (user: User) => void;
+}
+
+const UserForm: React.FC<Props> = ({onSubmitFormToAddUser}) => {
+  const initialFormState: UserMutation = {
+    name: '',
+    email: '',
+    active: false,
+    role: 'user',
+  };
+
+  const [form, setForm] = useState<UserMutation>(initialFormState);
+
+  const changeActive = () => {
+    setForm(prevForm => ({...prevForm, active: !form.active}));
+  }
+
+  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const {value, name} = e.target;
+    setForm(prevForm => ({...prevForm, [name]: value}));
+  }
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmitFormToAddUser({id: String(new Date().toISOString()), ...form});
+    console.log(form);
+    setForm(initialFormState)
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h4>User form</h4>
+      <hr/>
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="form-control"
+          value={form.name}
+          onChange={inputChangeHandler}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="form-control"
+          value={form.email}
+          onChange={inputChangeHandler}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="active">Active</label>
+        <input
+          type="checkbox"
+          id="active"
+          checked={form.active}
+          onChange={changeActive}
+        />
+      </div>
+
+      <div className="form-group">
+        <select name="role"
+                value={form.role}
+                onChange={inputChangeHandler}>
+          <option value="user">User</option>
+          <option value="editor">Editor</option>
+          <option value="admin">Admin</option>
+        </select>
+      </div>
+
+      <button type="submit" className="btn btn-primary mt-4">Add</button>
+    </form>
+  );
+};
+
+
+export default UserForm;
+
+
+
